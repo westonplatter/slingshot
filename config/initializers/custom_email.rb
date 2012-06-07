cfd4a8521d2e36305b35a08eb2bac331c6ec20e8
westonplatter <westonplatter@gmail.com>
2012-06-07 16:41:21 -0400

    added help email address

---
+++ config/initializers/custom_email.rb
@@ -0,0 +1,8 @@
+
+# WARNING
+
+# constants to be used throughout the applicaiton
+# note, they take precedence over those in ../application.rb
+# if constants are conflicting. Therefore, constantize with care.
+
+EMAIL_HELP="weston.platter@cojourners.com"
