02c445b2b7153bb2ac84890a20dfbecbe9bdc4a2
westonplatter <westonplatter@gmail.com>
2012-06-10 12:21:24 -0500

    added subject in the email href

--- config/initializers/custom_email.rb
+++ config/initializers/custom_email.rb
@@ -5,4 +5,10 @@
 # note, they take precedence over those in ../application.rb
 # if constants are conflicting. Therefore, constantize with care.
 
-EMAIL_HELP="weston.platter@cojourners.com"
+# <A HREF="mailto:name@domain.net?subject=Whatever"> name@domain.net </A>
+
+_email_address = "weston.platter@cojourners.com"
+_app_name = Rails.application.class.to_s.split("::").first
+_app_name_formatted = "[app help " + _app_name + "]"
+
+EMAIL_HELP = "mailto:" + _email_address + "?subject=" + _app_name_formatted
