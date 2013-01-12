f298eb5cecea15ed81b98940b2c6410c7447f489
Weston Platter <westonplatter@gmail.com>
2013-01-11 22:35:40 -0600

    the beginning of awesome

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slingshot::Application.config.secret_token = '185001bd67bac82fcda9b77c54d7b9a66e46f8e5f0fcb0e09d00b4f289ea987cb99b62784aa1c7065529da2190c8383c1fb5eb9fc22a98bb77ba92a5935daadb'
