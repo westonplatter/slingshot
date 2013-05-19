302909de6235a5c22fdb3712a3cd84202e26d119
Weston Platter <westonplatter@gmail.com>
2013-05-19 15:49:39 -0500

    removed tables from old app code. these tables are present because migrations created them. migrations are the source of truth.

--- db/schema.rb
+++ db/schema.rb
@@ -46,34 +46,12 @@ ActiveRecord::Schema.define(:version => 20130511023204) do
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
 
-  create_table "as", :force => true do |t|
-    t.string   "title"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "devices", :force => true do |t|
     t.integer  "user_id"
     t.string   "name"
     t.integer  "device_type"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
-  end
-
-  create_table "events", :force => true do |t|
-    t.string   "name"
-    t.text     "description"
-    t.date     "start_date"
-    t.date     "end_date"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-  end
-
-  create_table "posts", :force => true do |t|
-    t.string   "title"
-    t.string   "content"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
