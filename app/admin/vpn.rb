383dfe7605fffabf190addbcedb693522d4b5125
Weston Platter <westonplatter@gmail.com>
2013-08-13 17:35:39 -0500

    code up vpn changes to Henry

--- app/admin/vpn.rb
+++ app/admin/vpn.rb
@@ -7,6 +7,9 @@ ActiveAdmin.register Vpn do
     column :password
     column :old_username
     column :old_password
+    column "Region" do |vpn|
+      vpn.user.try(:location).try(:region).try(:name)
+    end
     
     default_actions
   end
@@ -15,9 +18,18 @@ ActiveAdmin.register Vpn do
     column "Owner Email" do |device|
       device.user.email
     end
+    column "First name" do |device|
+      device.user.first_name
+    end
+    column "Last name" do |device|
+      device.user.last_name
+    end
     column :password
     column :old_username
     column :old_password
+    column "Region" do |vpn|
+      vpn.user.try(:location).try(:region).try(:name)
+    end
   end  
 end
 
