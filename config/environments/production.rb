970753ff48e5227f2841b6b021a93112f2708871
Weston Platter <westonplatter@gmail.com>
2013-08-13 18:21:00 -0500

    setup production email for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,4 +69,14 @@ Slingshot::Application.configure do
   
   # force ssl for everything
   config.force_ssl = true
+  
+  ActionMailer::Base.smtp_settings = {
+    :port =>           '587',
+    :address =>        'smtp.mandrillapp.com',
+    :user_name =>      ENV['MANDRILL_USERNAME'],
+    :password =>       ENV['MANDRILL_APIKEY'],
+    :domain =>         'heroku.com',
+    :authentication => :plain
+  }
+  ActionMailer::Base.delivery_method = :smtp
 end
