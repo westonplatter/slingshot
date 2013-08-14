e2c7adf605692d35170f7be021bef655120d1e76
Weston Platter <westonplatter@gmail.com>
2013-08-13 21:20:44 -0500

    fix Postgresql's inability to convert string to integer

--- db/migrate/20130814012425_change_status_on_devices.rb
+++ db/migrate/20130814012425_change_status_on_devices.rb
@@ -1,6 +1,11 @@
 class ChangeStatusOnDevices < ActiveRecord::Migration
-  def change
+  def up
     rename_column :devices, :status_id, :status
     change_column :devices, :status, :string
+  end
+  
+  def down
+    remove_column :devices, :status
+    add_column :devices, :status_id, :integer
   end
 end
